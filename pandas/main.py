4aeae3a4c5baac0cc52cb81259697bca0adf4dc5
Jonathan <jonathancarranza70@gmail.com>
2024-10-31 00:46:03 -0300

    holjaldre

--- pandas/main.py
+++ pandas/main.py
@@ -1,3 +1,19 @@
 import pandas as pd
 
-print ('hola como estas')
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat',
+'dog', 'dog'],
+'age': [2.5, 3, 0.5, None, 5, 2, 4.5, None, 7, 3],
+'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no',
+'no']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data, index = labels )
+#print(df.iloc[:5])   desde cual y hasta cual
+#print(df.head(2))  #devuelve las columnas q le pedimos 
+#print(df.loc[:,['age', 'visits']])  puedo elegir que columnas mostrar
+print(df.loc[['a', 'c'],['age', 'visits']])
+
+
+
+#print(df)
